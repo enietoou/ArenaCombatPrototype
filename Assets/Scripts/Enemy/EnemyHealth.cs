ad2fa9b6eafe5ccdbf8cd8b10f60c355a4c34ac9
Азизджон <enietogou@gmail.com>
2026-03-03 09:35:17 +0500

    Add shooting and enemy

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour, IDamageable
+{
+    [SerializeField] private int maxHealth = 100;
+    
+    private int _currentHealth;
+
+    private void Awake()
+    {
+        _currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        _currentHealth -= amount;
+
+        if (_currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
