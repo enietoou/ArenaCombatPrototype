40563346fd14517bc2d145aedbb28f8cfec68168
Азизджон <enietogou@gmail.com>
2026-03-10 14:46:14 +0500

    Add pause menu with resume, main menu, quit and volume control

---
+++ Assets/Scripts/UI/PauseMenuUI.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenuUI : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private Slider volumeSlider;
+
+    private bool _isPaused;
+
+    private void Start()
+    {
+        pausePanel.SetActive(false);
+        
+        volumeSlider.value = AudioListener.volume;
+        volumeSlider.onValueChanged.AddListener(SetVolume);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    private void TogglePause()
+    {
+        _isPaused = !_isPaused;
+        
+        pausePanel.SetActive(_isPaused);
+        
+        Time.timeScale = _isPaused ? 0f : 1f;
+    }
+
+    public void Resume()
+    {
+        _isPaused = false;
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+
+    private void SetVolume(float value)
+    {
+        AudioListener.volume = value;
+    }
+}
